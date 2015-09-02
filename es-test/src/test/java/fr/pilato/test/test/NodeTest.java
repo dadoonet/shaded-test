68b5e3031a415cca78ede271b625f980b9302d57
David Pilato <david@pilato.fr>
2015-09-02 09:00:23 +0200

    Update with real unit tests

--- es-test/src/test/java/fr/pilato/test/test/NodeTest.java
+++ es-test/src/test/java/fr/pilato/test/test/NodeTest.java
@@ -24,16 +24,29 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.joda.time.DateTime;
+import org.elasticsearch.index.IndexNotFoundException;
 import org.junit.Test;
 
 import java.net.InetSocketAddress;
 import java.security.CodeSource;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class NodeTest {
 
     @Test
-    public void testShadedNode() {
+    public void testJodaLibVersion() {
+        CodeSource codeSource = new org.joda.time.DateTime().getClass().getProtectionDomain().getCodeSource();
+        assertThat(codeSource.getLocation().getFile(), containsString("joda-time-2.1.jar"));
+
+        codeSource = new fr.pilato.thirdparty.joda.time.DateTime().getClass().getProtectionDomain().getCodeSource();
+        assertThat(codeSource.getLocation().getFile(), containsString("es-shaded-1.0-SNAPSHOT.jar"));
+    }
+
+    @Test
+    public void testShadedClient() {
         TransportClient client = TransportClient.builder()
                 .settings(Settings.builder()
                                 .put("path.home", ".")
@@ -43,14 +56,17 @@ public class NodeTest {
                 )
                 .build();
         client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("localhost", 9300)));
-        ClusterHealthResponse health = client.admin().cluster().prepareHealth().get();
-        System.out.println("getClusterName = " + health.getClusterName());
+        ClusterHealthResponse health = client.admin().cluster().prepareHealth().setWaitForYellowStatus().get();
+        assertThat(health.getClusterName(), is("elasticsearch"));
 
-        CodeSource codeSource = new DateTime().getClass().getProtectionDomain().getCodeSource();
-        System.out.println("codeSource = " + codeSource);
+        try {
+            client.admin().indices().prepareDelete("test").get();
+        } catch (IndexNotFoundException e) {
+            // That's fine
+        }
 
         client.prepareIndex("test", "doc", "1").setSource("foo", "bar").setRefresh(true).get();
         SearchResponse searchResponse = client.prepareSearch("test").get();
-        System.out.println("totalHits = " + searchResponse.getHits().getTotalHits());
+        assertThat(searchResponse.getHits().getTotalHits(), is(1L));
     }
 }
